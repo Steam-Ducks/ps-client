a0aeffbb50afc63d0f1c2e54e09ac599fb7a763f
Luiz Henrique Rabello Ferreira <91166115+LuizHRFerreira@users.noreply.github.com>
2025-04-07 21:21:44 -0300

    feat(PS-39): add Select2 search box for company selection

--- ps-client/src/components/employee/EmployeeList.vue
+++ ps-client/src/components/employee/EmployeeList.vue
@@ -1,19 +1,10 @@
 <template>
   <div v-if="employees && employees.length > 0" class="employee-list">
-    <table>
-      <thead>
-        <tr>
-          <th></th>
-          <th>Nome Completo</th>
-          <th>CPF</th>
-          <th>Cargo</th>
-          <th> Empresa </th>
-        </tr>
-      </thead>
-      <tbody>
-        <EmployeeItem v-for="employee in employees" :key="employee.id" :employee="employee" />
-      </tbody>
-    </table>
+    <DataTable
+      :data="formattedemployees"
+      :columns="columns"
+      :options="tableOptions"
+    />
   </div>
   <div v-else class="no-employee-message">
     <p>Nenhum funcionário encontrado. Tente buscar novamente.</p>
@@ -21,12 +12,13 @@
 </template>
 
 <script>
-import EmployeeItem from './EmployeeItem.vue';
+import DataTable from 'datatables.net-vue3';
+import DataTablesCore from 'datatables.net';
 
 export default {
   name: 'EmployeeList',
   components: {
-    EmployeeItem,
+    DataTable,
   },
   props: {
     employees: {
@@ -34,7 +26,57 @@ export default {
       required: true,
     },
   },
-};
+  data() {
+      return {
+        tableKey: 0,
+        columns: [
+          { title: ' ',
+            data: 'foto',
+            orderable: false, 
+            render: (data) => {
+              return `<img src="${data}" alt="Foto de perfil" class="photo"/>`;
+            },
+            class:'image'
+          },
+          { title: 'Nome completo', data: 'name', class:'name' },
+          { title: 'CPF', data: 'cpf', class:'cpf' },
+          { title: 'Cargo', data: 'position', class:'position' },
+          { title: 'Empresa', data: 'company', class:'company' },
+        ],
+        tableOptions: {
+          responsive: true,
+          language: {
+            url: '//cdn.datatables.net/plug-ins/1.13.6/i18n/pt-BR.json',
+          },
+          dom: '<"top"f>rt<"bottom"lip><"clear">',
+        },
+      };
+    },
+    computed: {
+      formattedemployees() {
+        return this.employees.map((employee) => ({
+          foto: employee.photo,
+          name: employee.name,
+          cpf: employee.cpf,
+          position: employee.position.name,
+          company: employee.company.name,
+        }));
+      },
+    },
+    mounted() {
+      DataTable.use(DataTablesCore);
+    },
+    watch: {
+      employee() {
+        this.tableKey += 1;
+      },
+    },
+    beforeUnmount() {
+      if (this.$refs.dataTable?.dt) {
+        this.$refs.dataTable.dt.destroy(true);
+      }
+    },
+  };
 </script>
 
 <style scoped>
@@ -48,5 +90,12 @@ export default {
   margin-top: 20px;
 }
 
+.img {
+  width: 44px;
+  height: 44px;
+  object-fit: cover;
+  object-position: center;
+  border-radius: 50%;
+}
 
 </style>
--- ps-client/src/components/users/UserForm.vue
+++ ps-client/src/components/users/UserForm.vue
@@ -1,37 +1,48 @@
 <template>
-  <div class="user-form">
-    <h2 class="form-title">Cadastrar Novo Usuário</h2>
     <form @submit.prevent="submitForm">
       <div class="form-group">
-        <label for="username">Nome:</label>
-        <input type="text" id="username" v-model="user.username" required />
-      </div>
-      <div class="form-group">
-        <label for="email">Email:</label>
-        <input type="email" id="email" v-model="user.email" required />
-      </div>
-      <div class="form-group">
-        <label for="password">Senha:</label>
-        <input type="password" id="password" v-model="user.password" required />
-      </div>
+
+        <!-- nome -->
+        <div class="form-group">
+          Nome:
+          <input type="text" id="username" v-model="user.username" required />
+        </div>
+
+        <!-- email -->
+        <div class="form-group">
+          Email:
+          <input type="email" id="email" v-model="user.email" required />
+        </div>
+
+        <!-- senha -->
+        <div class="form-group">
+          Senha:
+          <input type="password" id="password" v-model="user.password" required />
+        </div>
+
       <FormButton type="primary" class="submit-button">Cadastrar</FormButton>
+      
       <div v-if="errorMessage" class="error-message">
         {{ errorMessage }}
       </div>
-    </form>
-  </div>
+
+    </div>
+  </form>
 </template>
 
 <script>
 import UserService from '@/services/UserService';
 import FormButton from '@/components/ui/FormButton.vue';
+import Swal from 'sweetalert2';
 
 export default {
   name: 'UserForm',
   components: {
     FormButton, 
   },
+  
   emits: ['user-created'],
+
   data() {
     return {
       user: {
@@ -42,65 +53,97 @@ export default {
       errorMessage: '',
     };
   },
+
   methods: {
     async submitForm() {
+
       this.errorMessage = ''; 
+
       try {
         await UserService.createUser(this.user);
+
+        Swal.fire({
+          icon: 'success',
+          title: 'Usuário cadastrada com sucesso!',
+          showConfirmButton: false,
+          timer: 1500,
+        }).then(() => {
         this.user = {
           username: '',
           email: '',
           password: '',
         };
+      });
+
         this.$emit('user-created');
       } catch (error) {
-        this.errorMessage = 'Erro ao cadastrar usuário. Tente novamente.';
-        console.error('Erro ao cadastrar usuário:', error);
+        Swal.fire({
+          icon: 'error',
+          title: 'Erro ao cadastar Empresa',
+          text: error.message,
+          showConfirmButton: false,
+          timer: 3500,
+        });
       }
+    },
+    showErrorAlert(errorMessage) {
+      Swal.fire({
+        icon: 'warning',
+        title: 'Faltam informações',
+        text: errorMessage,
+        showConfirmButton: false,
+        timer: 2500,
+      });
     },
   },
 };
 </script>
 
 <style scoped>
-.user-form {
-  max-width: 500px;
-  margin: 0 auto;
-  padding: 30px;
-  background-color: #ffffff;
-  border-radius: 10px;
-  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
-}
+  .company-form {
+    max-width: 500px;
+    margin: 0 auto;
+    padding: 30px;
+    background-color: #ffffff;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
 
-.form-title {
-  text-align: center;
-  color: #1e293b;
-  margin-bottom: 30px;
-}
+  .form-title {
+    text-align: center;
+    color: #1e293b;
+    margin-bottom: 30px;
+  }
 
-.form-group {
-  margin-bottom: 20px;
-}
+  .form-group {
+    margin-bottom: 20px;
+    width: 100%;
+  }
 
-label {
-  display: block;
-  margin-bottom: 8px;
-  color: #334155;
-}
+  label {
+    display: block;
+    margin-bottom: 8px;
+    color: #334155;
+  }
 
-input[type='text'],
-input[type='email'],
-input[type='password'] {
-  width: 100%;
-  padding: 12px;
-  border: 1px solid #cbd5e1;
-  border-radius: 6px;
-  font-size: 1rem;
-  box-sizing: border-box;
-}
+  input[type='text'],
+  input[type='email'], 
+    input[type='password'] {
+    width: 100%;
+    padding: 12px;
+    border: 1px solid #cbd5e1;
+    border-radius: 6px;
+    font-size: 1rem;
+    box-sizing: border-box;
+  }
 
-.error-message {
-  color: #dc2626;
-  margin-top: 15px;
-}
+  .error-message {
+    color: #dc2626;
+    margin-top: 15px;
+  }
+
+  .button-container {
+    margin-top: 20px;
+  }
 </style>
