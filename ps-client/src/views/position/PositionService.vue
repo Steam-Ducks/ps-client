17ceff3b7a6dd1bb04c48ead1cd0c16dbd361af7
ahlesk <lima.alexander.las@gmail.com>
2025-04-25 18:06:54 -0300

    feat (PS-45):Job registration page created

---
+++ ps-client/src/views/position/PositionService.vue
@@ -0,0 +1,16 @@
+import axios from 'axios';
+
+export default {
+  async getAllCargos() {
+    const response = await axios.get('/api/cargos');
+    return response.data;
+  },
+
+  async createCargo(cargo) {
+    await axios.post('/api/cargos', cargo);
+  },
+
+  async updateCargo(id, cargo) {
+    await axios.put(`/api/cargos/${id}`, cargo);
+  }
+};
